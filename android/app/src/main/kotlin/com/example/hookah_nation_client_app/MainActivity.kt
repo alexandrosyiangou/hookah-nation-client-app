abe4fb57b8884558acfa32073511dee668bee3cf
Alexandros Yiangou <alexyiangousolutions@gmail.com>
2020-02-17 11:25:50 +0200

    :construction: Lauuuunching :construction:

---
+++ android/app/src/main/kotlin/com/example/hookah_nation_client_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hookah_nation_client_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
